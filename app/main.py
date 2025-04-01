d6fada311f3592c8e4a2e3edd2651a9b554e35ab
Orest Yakymiv <akimivorest@gmail.com>
2025-04-01 16:58:32 +0300

    create function move which check directory and file, after check rename file or move the file in the directory

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+import shutil
+from os import makedirs
+from os.path import isfile
+
+
+def move_file(command) -> None:
+    split_command = command.split()
+    if len(split_command) != 3 or split_command[0] != "mv":
+        return
+
+    source_filename, target_filename = split_command[1], split_command[2]
+    if not os.path.exists(source_filename):
+        return
+
+    if isfile(target_filename):
+        shutil.move(source_filename, target_filename)
+        return
+
+    target_dirname = os.path.dirname(target_filename)
+    if target_dirname and not os.path.exists(target_dirname):
+        makedirs(target_dirname, exist_ok=True)
+
+    shutil.move(source_filename, target_filename)
